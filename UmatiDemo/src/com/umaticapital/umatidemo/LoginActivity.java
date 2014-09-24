8cacc143f7e80f5d83554a69b3fbdd406b7be714
Akaguara <akaguara@gmail.com>
2014-09-24 13:16:52 -0400

    first commit

---
+++ UmatiDemo/src/com/umaticapital/umatidemo/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.umaticapital.umatidemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LoginActivity extends Activity{
+	
+	Button mLogin;
+	Button mSignUp;
+	
+	String username;
+	String pasword;
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+
+}
